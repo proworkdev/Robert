032bfc3fcb3e878a7f31b2fe069b78eab74f7242
proworkdev <proworkdev@gmail.com>
2019-02-13 13:38:32 +0530

    db changes

--- backend/src/config.js
+++ backend/src/config.js
@@ -8,6 +8,6 @@ if(process.env.NODE_ENV != 'production'){
 module.exports = {
   jwt_secret: process.env.JWT_SECRET || 'unsafe_jwt_secret',
   mongoose: {
-    uri: process.env.MONGODB_URI || 'mongodb://localhost/mern-stack'
+    uri: process.env.MONGODB_URI || 'mongodb://admin12:admin12@ds133875.mlab.com:33875/mern-stack'
   },
 }
